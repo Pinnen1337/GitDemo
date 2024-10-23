6799c5c6f7c635f6529eb76a4764ef5261d8a21f
Christoffer Johansson <c.johansson90@live.se>
2024-10-23 13:07:36 +0200

    Added day info

--- GitDemo/GitDemo/Program.cs
+++ GitDemo/GitDemo/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Helpers.WriteText("Hejsan världen igen!");
+            Console.WriteLine("Idag är det onsdag!");
             Helpers.MyLoop();
         }
     }
